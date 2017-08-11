4bdf83b7a5ee460cdf5f144083364b1c5063986b
Michael Mamadilov <mmamadilov@cybervisiontech.com>
2017-08-11 13:12:22 +0300

    Initial UI structure

---
+++ mapr-ui/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { MaprUiPage } from './app.po';
+
+describe('mapr-ui App', () => {
+  let page: MaprUiPage;
+
+  beforeEach(() => {
+    page = new MaprUiPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
