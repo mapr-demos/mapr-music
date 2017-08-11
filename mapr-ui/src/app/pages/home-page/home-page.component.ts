7b1f3b6194d850a52913d1f2294275d5ef2084c3
Michael Mamadilov <mmamadilov@cybervisiontech.com>
2017-08-11 14:59:01 +0300

    #4 Fixed issue with URL navigation

--- mapr-ui/src/app/pages/home-page/home-page.component.ts
+++ mapr-ui/src/app/pages/home-page/home-page.component.ts
@@ -2,38 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import {AlbumService} from "../../services/album.service";
 import {Album} from '../../models/Album';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgbPaginationConfig } from '@ng-bootstrap/ng-bootstrap';
 import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePage implements OnInit{
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private albumService: AlbumService
-  ) {}
+    private albumService: AlbumService,
+    private config: NgbPaginationConfig
+  ) {
+    config.maxSize = 5;
+  }
 
   albums: Array<Album> = [];
-  totalAlbums: number = 0;
-  pageNumber: number = 1;
+  totalAlbums: number;
+  pageNumber: number;
 
   ngOnInit(): void {
     this.route.queryParams
       .switchMap(({page = 1}) => {
-        this.pageNumber = page;
-        return this.albumService.getPage(page)
+        return this.albumService.getPage(page).then((albumsPage) => [albumsPage, page]);
       })
-      .subscribe(({albums, totalNumber}) => {
+      .subscribe(([{albums, totalNumber}, page]) => {
+        this.pageNumber = page;
         this.albums = albums;
         this.totalAlbums = totalNumber;
-        console.log(this.totalAlbums);
       });
   }
 
-  onChangePage() {
-    this.router.navigate(['/'], {queryParams: {page: this.pageNumber}});
+  onChangePage(newPage: number) {
+    if (!Number.isNaN(newPage)) {
+      this.pageNumber = newPage;
+      this.router.navigate(['/'], {queryParams: {page: this.pageNumber}});
+    }
   }
 }
