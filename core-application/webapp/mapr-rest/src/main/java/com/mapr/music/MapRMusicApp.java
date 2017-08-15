c06b84ceac7afbd0316752419263097d8b836e51
vladglinskiy <vglinskiy@cybervisiontech.com>
2017-08-15 14:12:34 +0300

    issue #9 : Initial backend app

---
+++ core-application/webapp/mapr-rest/src/main/java/com/mapr/music/MapRMusicApp.java
@@ -0,0 +1,9 @@
+package com.mapr.music;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("/api/1.0/")
+public class MapRMusicApp extends Application {
+}
